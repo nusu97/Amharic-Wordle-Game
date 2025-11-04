5c25df647f58636feb2f96293206c0e75a4b4b18
nusu97 <nuseybaahmed33@gmail.com>
2025-11-04 08:58:39 -0500

    create a class LetterState that holds the charactyere and whether or not it is the right word in the right position

---
+++ letter_state.py
@@ -0,0 +1,5 @@
+class LetterState:
+    def __int__(self, character: str):
+        self.character: str = character
+        self.is_in_word_word: bool = False
+        self.is_in_position: bool = FalseS

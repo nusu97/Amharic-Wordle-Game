815e743c382bed751efa3392dcd69ce900e9ccfd
nusu97 <nuseybaahmed33@gmail.com>
2025-11-04 08:46:24 -0500

    we faioled to solve the puzzle so add else condition to check

--- play_wordle.py
+++ play_wordle.py
@@ -3,13 +3,16 @@ from wordle import Wordle
 def main():
     print("Hello World") 
     wordle = Wordle("APPLE")
-    print(wordle)
+    
 
     while wordle.can_attempt:
         x = input("Type your guess: ")
-        wordle.attempts(x)
+        wordle.attempt(x)
+
     if wordle.is_solved:
         print("you have solved the puzzle.")
+    else:
+        print("you failed to sdolve the puzzle!")
 
 if __name__ == "__main__":
     main()

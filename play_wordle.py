f3806e1ad2d5089c23fbefa8b410326d138142e8
nusu97 <nuseybaahmed33@gmail.com>
2025-11-04 08:04:39 -0500

    Create a loop to check if the guessed word matches the correct 5 letter word. If not, print “your gess is incorrect” Give the user 6 attempts.

--- play_wordle.py
+++ play_wordle.py
@@ -2,8 +2,14 @@ from wordle import Wordle
 
 def main():
     print("Hello World") 
-    wordle = Wordle()
+    wordle = Wordle("APPLE")
     print(wordle)
 
+    while True:
+        x = input("Type your guess: ")
+        if x == wordle.secret:
+            print("you have guessed the word!")
+            break
+        print("your guess is incorrect")
 if __name__ == "__main__":
     main()

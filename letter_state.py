f5779d8f473365c9813b48a0be78be4d0b748dae
nusu97 <nuseybaahmed33@gmail.com>
2025-11-04 09:15:36 -0500

    return actual characters and status of position in description

--- letter_state.py
+++ letter_state.py
@@ -1,5 +1,8 @@
 class LetterState:
     def __int__(self, character: str):
         self.character: str = character
-        self.is_in_word_word: bool = False
-        self.is_in_position: bool = FalseS
+        self.is_in_word: bool = False
+        self.is_in_position: bool = False
+
+    def __repr__(self):
+        return f"[{self.character} is_in_word: {self.is_in_word} is_in_position: {self.is_in_position}]"

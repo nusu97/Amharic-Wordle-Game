730512fe32ebb31b2bbb1bf9d99b6b160cf179c6
nusu97 <nuseybaahmed33@gmail.com>
2025-11-04 07:39:14 -0500

    intialize classes

---
+++ play_wordle.py
@@ -0,0 +1,9 @@
+from wordle import Wordle
+
+def main():
+    print("Hello World") 
+    wordle = Wordle()
+    print(wordle)
+
+if __name__ == "__main__":
+    main()
---
+++ wordle.py
@@ -0,0 +1,4 @@
+class Wordle:
+    
+    def __init__(self):
+        pass

64ac75d91380ab91928df9d8e5e031afece9119d
nusu97 <nuseybaahmed33@gmail.com>
2025-11-04 09:08:49 -0500

    add a guess function to loop through each character in the secret word and check the word and position

--- wordle.py
+++ wordle.py
@@ -1,5 +1,5 @@
+from letter_state import LetterState
 class Wordle:
-
     MAX_ATTEMPTS = 6
     WORD_LENGTH = 5
     
@@ -11,6 +11,17 @@ class Wordle:
     def attempt(self, word: str):
         self.attempts.append(word)
 
+    def guess(self, worrd: str):
+        result = []
+        
+        for i in range(self.WORD_LENGTH):
+            character = word[i]
+            letter = LetterState(character)
+            letter.is_in_word = character in self.secret
+            letter.is_in_position = character == self.secret[i]
+            result.append(letter)
+            
+        return result
     @property
     def is_solved(self):
         return len(self.attempts) > 0 and self.attempts[-1] == self.secret
